137425f3cf020de27c952fd972c0c2489ffa425b
ameerhamza056 <hamzaawaismughal@gmail.com>
2026-03-31 19:57:57 +0500

    Add files via upload

---
+++ src/Components/Html.jsx
@@ -0,0 +1,11 @@
+export default function Html(){
+    return(
+        <> <div class="main">
+    <div class="a"></div>
+    <div class="b"></div>
+    <div class="c"></div>
+    </div>
+    <h1>Flag of chad</h1>
+        </>
+    )
+}
